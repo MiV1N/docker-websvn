a5f6a0c6e5837280b0df2b288330452d0460347a
MiV1N <111258029+MiV1N@users.noreply.github.com>
2022-10-14 15:47:04 +0800

    配置 config.php 支持配置用户名和密码以及开启下载

--- config.php
+++ config.php
@@ -1,11 +1,14 @@
 <?php
 $repository = getenv('repository');
-$config->addRepository('Main', $repository);
+$username = getenv('username');
+$pwd = getenv('pwd');
+$config->addRepository('Main', $repository, null, $username, $pwd);
 $config->addTemplatePath($locwebsvnreal.'/templates/calm/');
 $config->addTemplatePath($locwebsvnreal.'/templates/BlueGrey/');
 $config->addTemplatePath($locwebsvnreal.'/templates/Elegant/');
 $config->addInlineMimeType('text/plain');
 $config->setMinDownloadLevel(2);
+$config->allowDownload();
 $config->useGeshi();
 set_time_limit(0);
 $config->expandTabsBy(8);
